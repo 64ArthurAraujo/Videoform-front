655caf60aff871bf2a63d6c4eb530a19e06a532f
arthur <arthur.araujo@tutanota.com>
2022-07-03 02:53:07 -0300

    Add inputs to test upload form

--- src/components/FileUploadForm.svelte
+++ src/components/FileUploadForm.svelte
@@ -2,11 +2,18 @@
     export let urlToSend: string = "";
 </script>
 
-<form method="POST" 
-    action="{urlToSend}" enctype='multipart/form-data'>
+<div class="flex items-center justify-center">
+    <form method="POST" action="{urlToSend}" enctype="multipart/form-data">
 
-    <input type="file" name="video"> <br>
-    <button class="bg-blue-600 hover:bg-blue-700 text-white rounded-md py-2 px-4 my-2">
-        Submit
-    </button>
-</form>
+        <input type="text" class="shadow mt-2" name="title" placeholder="title"> <br>
+        <input type="text" class="shadow mt-2" name="description" placeholder="description"> <br>
+        <input type="text" class="shadow mt-2" name="userToken" placeholder="user token"> <br>
+        <input type="text" class="shadow mt-2" name="categoryId" placeholder="category id"> <br>
+        
+        <input type="file" name="video">
+
+        <button class="bg-blue-600 hover:bg-blue-700 text-white rounded-md py-2 px-4 my-2">
+            Submit
+        </button>
+    </form>
+</div>
